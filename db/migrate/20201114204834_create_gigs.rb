cbcf5e74f744281c91c8a8b1a4cfa08dbd69671e
JamesLeggero <james.m.leggero@gmail.com>
2020-11-15 21:49:46 -0500

    gig schema

--- db/migrate/20201114204834_create_gigs.rb
+++ db/migrate/20201114204834_create_gigs.rb
@@ -3,8 +3,8 @@ class CreateGigs < ActiveRecord::Migration[6.0]
     create_table :gigs do |t|
       t.boolean :hidden
       t.string :band
-      t.string :venue
-      t.string :stream
+      t.string :venue_stream
+      t.string :v_s_link
       t.string :support
       t.date :date
       t.time :time
